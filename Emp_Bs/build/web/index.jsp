acc86008b8c054c3f74a323633c83e94d926eaf9
RAKSHIT SHAH <rakshitshah1994@gmail.com>
2015-08-17 00:32:40 +0530

    JSP AND SERVLET IN JAVA
    Employee - Basic Salary - fix salary : calculation using JSP and Servlet
    in JAVA.

---
+++ Emp_Bs/build/web/index.jsp
@@ -0,0 +1,21 @@
+<%-- 
+    Document   : index
+    Created on : Aug 16, 2015, 4:09:57 PM
+    Author     : hp-pc
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <form action="Bs_Calc" method="GET">
+            Enter basic salary : 
+            <input type="text" name="bs" /> <br>
+            <button type="submit" name="submit" value="Submit">Submit</button>
+        </form>
+    </body>
+</html>
---
+++ Emp_Bs/src/java/Bs_Calc.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import static java.lang.Integer.parseInt;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author hp-pc
+ */
+public class Bs_Calc extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+             if (request.getParameter("bs") != null) {
+                String bs = request.getParameter("bs");
+                Integer bs_dbl = parseInt(bs);
+
+                double fs = 0;
+                fs = bs_dbl + (bs_dbl * 0.4) + (bs_dbl * 0.1);
+                out.print("FIX Salary is : " +fs);
+            } else {
+                out.println("please enter valid amount");
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
